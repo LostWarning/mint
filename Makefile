aec24bdb8b59a47f3b64d8f881d74e0099012a7d
Amal C.S <amal4cs@gmail.com>
2023-03-26 12:38:27 +0530

    Build script changes Add clean Makefile target Add PHONY Makefile targer

--- Makefile
+++ Makefile
@@ -2,3 +2,8 @@ default: build
 
 build:
 	'$(CURDIR)/scripts/src.build.sh'
+
+clean:
+	'$(CURDIR)/scripts/src.clean.sh'
+
+.PHONY: default build clean
--- scripts/src.clean.sh
+++ scripts/src.clean.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 BDIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 
-
-
-pushd ${BDIR}/cmd/mint >> /dev/null
-
-go build -o "${BDIR}/bin/mint"
-
-popd >> /dev/null
+rm -rfv $BDIR/mint
+rm -rfv $BDIR/bin
